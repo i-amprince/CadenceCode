dcd0966fa21ea41f52ef921012a9cefffe0a7163
i-amprince <princeg1608@gmail.com>
2025-07-15 11:43:44 +0530

    added port in server

--- Server/server.js
+++ Server/server.js
@@ -208,5 +208,5 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
